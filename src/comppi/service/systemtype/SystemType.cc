478758101100f04a52c63d4e888663292b8dd22b
erenon <erenon2@gmail.com>
2013-09-26 19:10:58 +0200

    service SystemType::loadDatabase implemented, systemType csv sources added

--- src/comppi/service/systemtype/SystemType.cc
+++ src/comppi/service/systemtype/SystemType.cc
@@ -1,5 +1,17 @@
+#include <fstream>
+#include <stdexcept>
+
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/trim.hpp>
+
 #include <comppi/service/systemtype/SystemType.h>
 #include <comppi/service/config/Config.h>
+#include <comppi/service/log/Log.h>
+
+#include <comppi/entity/SystemType.h>
+#include <comppi/entity/gen/SystemType-odb.hxx>
+
+#include <comppi/utility/Tokenizer.h>
 
 namespace comppi {
 namespace service {
@@ -25,6 +37,48 @@ SystemType::SystemType(
      _synonymPath(synonymPath),
      _databasePtr(databasePtr)
 {}
+
+bool SystemType::loadDatabase() {
+    INFO << "Load SystemTypes from: " << _systemPath;
+
+    std::fstream input(_systemPath.c_str(), std::fstream::in);
+    utility::Tokenizer<2> tokenizer(input, {{0, 1}}, ';');
+
+    {
+        using namespace comppi::service::database;
+        Transaction transaction(_databasePtr->begin());
+
+        for (const auto& line : tokenizer) {
+            try {
+                int confidenceType = boost::lexical_cast<int>(line[1]);
+
+                entity::SystemType st(
+                    boost::trim_copy(line[0]),
+                    confidenceType
+                );
+
+//                TRACE << "Load [" << boost::trim_copy(line[0])
+//                    << ", " << confidenceType << "]";
+
+                _databasePtr->persist(st);
+            } catch (const boost::bad_lexical_cast& ex) {
+                ERROR << "System type named '" << line[0]
+                    << "' has invalid confidence type: "
+                    << ex.what();
+            } catch (const std::invalid_argument& ex) {
+                ERROR << "While loading system type named '" << line[0]
+                    << "': "
+                    << ex.what();
+            }
+        }
+
+        transaction.commit();
+    }
+
+    input.close();
+
+    return true;
+}
 
 } // namespace systemtype
 } // namespace service
