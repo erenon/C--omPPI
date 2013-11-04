ac42300912f559c44079a6544d206f8020def7c4
erenon <erenon2@gmail.com>
2013-11-04 22:08:26 +0100

    comppi.cpp: change default controller to Interaction

--- src/comppi/comppi.cpp
+++ src/comppi/comppi.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
+#include <vector>
+
+#include <boost/program_options.hpp>
 
 #include <comppi/service/Container.h>
 #include <comppi/service/config/Config.h>
+#include <comppi/service/log/Log.h>
 
-#include <comppi/entity/SystemType.h>
-#include <comppi/entity/gen/SystemType-odb.hxx>
-#include <comppi/service/database/Database.h>
+#include <comppi/controller/Map.h>
+#include <comppi/controller/Interaction.h>
 
 int main(int argc, char* argv[]) {
-//    comppi::entity::SystemType st("bar", comppi::entity::SystemType::EXPERIMENTAL);
-//
-//    std::cout << st.getName() << std::endl;
-//
-//    {
-//        using namespace comppi::service::database;
-//        DatabasePtr db(createDatabase());
-//
-//        Transaction transaction(db->begin());
-//
-//        db->persist(st);
-//
-//        std::cout << st.getId() << std::endl;
-//
-//        transaction.commit();
-//    }
     using comppi::service::Container;
     Container container;
 
     using comppi::service::config::Config;
 
-    {
+    {   // bootstrap config
         auto config = container.get<Config>();
 
         // read config
@@ -38,13 +25,79 @@ int main(int argc, char* argv[]) {
         if (!status) {
             return 1;
         }
+
+        // TODO make this configurable from cli
+        config->setTransformation("comppi_root", ".");
     }
+
+//    {   // TODO bootstrap logging
+//        auto config = container.get<Config>();
+//        std::cout << config->get("log.level") << std::endl;
+//    }
+
+//    {
+//        using comppi::service::systemtype::SystemType;
+//        auto stPtr = container.get<SystemType>();
+//        stPtr->loadDatabase();
+//    }
+//    { // init program options
+//        using namespace boost::program_options;
+//
+//        options_description generic("Allowed options");
+//        generic.add_options()
+//            ("help,h", "Show this help")
+//        ;
+//
+//        options_description command("Command and options");
+//        command.add_options()
+//            ("command", value< std::vector<std::string> >(), "Command and arguments")
+//        ;
+//
+//        options_description cli("CLI commands");
+//        cli.add(generic).add(command);
+//
+//        positional_options_description p;
+//        p.add("command", -1);
+//
+//        variables_map vm;
+//
+//        try {
+//            store(
+//                command_line_parser(argc, argv)
+//                    .options(cli)
+//                    .positional(p).run(),
+//                vm
+//            );
+//        } catch (const unknown_option& ex) {
+//            std::cout << ex.what() << std::endl
+//                << generic << std::endl;
+//        }
+//        //notify(vm);
+//
+//        if (vm.count("help")) {
+//            std::cout << generic << std::endl;
+//            return 1;
+//        }
+//
+//        if (vm.count("command")) {
+//            std::cout << vm["command"].as< std::vector<std::string> >()[0]
+//                << std::endl;
+//        }
+//    }
+
+//    {
+//        using comppi::controller::Map;
+//        Map mapController(container);
+//        std::vector<std::string> inputs;
+//        mapController.build(inputs);
+//    }
 
     {
-        auto config = container.get<Config>();
-        std::cout << config->get("log.level") << std::endl;
+        using comppi::controller::Interaction;
+        Interaction controller(container);
+        std::vector<std::string> inputs;
+        controller.build(inputs);
     }
-
 
     return 0;
 }
