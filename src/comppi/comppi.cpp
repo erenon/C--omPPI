a48645b6fb92e223668148f7043f898e958e4685
erenon <erenon2@gmail.com>
2013-11-06 23:51:39 +0100

    ugly command dispatcher implemented

--- src/comppi/comppi.cpp
+++ src/comppi/comppi.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include <boost/program_options.hpp>
@@ -6,6 +7,7 @@
 #include <comppi/service/Container.h>
 #include <comppi/service/config/Config.h>
 #include <comppi/service/log/Log.h>
+#include <comppi/service/systemtype/SystemType.h>
 
 #include <comppi/controller/Map.h>
 #include <comppi/controller/Interaction.h>
@@ -31,80 +33,113 @@ int main(int argc, char* argv[]) {
         config->setTransformation("comppi_root", ".");
     }
 
+    { // init program options
+        using namespace boost::program_options;
+
+        options_description generic("Allowed options");
+        generic.add_options()
+            ("help,h", "Show this help")
+            ("verbose,v", "Be verbose")
+            ("moreverbose,V", "Be more verbose")
+        ;
+
+        options_description command("Command and options");
+        command.add_options()
+            ("command", value<std::string>(), "Command")
+            ("subcommand", value<std::string>(), "Subcommand")
+            ("inputs", value< std::vector<std::string> >(), "Inputs")
+        ;
+
+        options_description cli("CLI commands");
+        cli.add(generic).add(command);
+
+        positional_options_description p;
+        p.add("command", 1);
+        p.add("subcommand", 1);
+        p.add("inputs", -1);
+
+        variables_map vm;
+
+        try {
+            store(
+                command_line_parser(argc, argv)
+                    .options(cli)
+                    .positional(p).run(),
+                vm
+            );
+        } catch (const unknown_option& ex) {
+            std::cout << ex.what() << std::endl
+                << cli << std::endl;
+        }
+        //notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << cli << std::endl;
+            return 0;
+        }
+
 //    {   // TODO bootstrap logging
 //        auto config = container.get<Config>();
 //        std::cout << config->get("log.level") << std::endl;
 //    }
 
-//    {
-//        using comppi::service::systemtype::SystemType;
-//        auto stPtr = container.get<SystemType>();
-//        stPtr->loadDatabase();
-//    }
-//    { // init program options
-//        using namespace boost::program_options;
-//
-//        options_description generic("Allowed options");
-//        generic.add_options()
-//            ("help,h", "Show this help")
-//        ;
-//
-//        options_description command("Command and options");
-//        command.add_options()
-//            ("command", value< std::vector<std::string> >(), "Command and arguments")
-//        ;
-//
-//        options_description cli("CLI commands");
-//        cli.add(generic).add(command);
-//
-//        positional_options_description p;
-//        p.add("command", -1);
-//
-//        variables_map vm;
-//
-//        try {
-//            store(
-//                command_line_parser(argc, argv)
-//                    .options(cli)
-//                    .positional(p).run(),
-//                vm
-//            );
-//        } catch (const unknown_option& ex) {
-//            std::cout << ex.what() << std::endl
-//                << generic << std::endl;
-//        }
-//        //notify(vm);
-//
-//        if (vm.count("help")) {
-//            std::cout << generic << std::endl;
-//            return 1;
-//        }
-//
-//        if (vm.count("command")) {
-//            std::cout << vm["command"].as< std::vector<std::string> >()[0]
-//                << std::endl;
-//        }
-//    }
+        // TODO show proper help messages
+        // e.g: list available commands
 
-//    {
-//        using comppi::controller::Map;
-//        Map mapController(container);
-//        std::vector<std::string> inputs;
-//        mapController.build(inputs);
-//    }
+        if (vm.count("command")) {
+            std::cout << vm["command"].as<std::string>() << std::endl;
 
-//    {
-//        using comppi::controller::Interaction;
-//        Interaction controller(container);
-//        std::vector<std::string> inputs;
-//        controller.build(inputs);
-//    }
+            std::string command = vm["command"].as<std::string>();
 
-    {
-        using comppi::controller::Localization;
-        Localization controller(container);
-        std::vector<std::string> inputs;
-        controller.build(inputs);
+            if (command == "build") {
+                if (vm.count("subcommand")) {
+                    std::string subcommand = vm["subcommand"].as<std::string>();
+                    std::vector<std::string> inputs;
+                    if (vm.count("inputs")) {
+                        inputs = vm["inputs"].as<std::vector<std::string>>();
+                    }
+
+                    if (subcommand == "systemtype")
+                    {
+                        using comppi::service::systemtype::SystemType;
+                        auto stPtr = container.get<SystemType>();
+                        stPtr->loadDatabase();
+                    }
+                    else if (subcommand == "map")
+                    {
+                        using comppi::controller::Map;
+                        Map mapController(container);
+                        mapController.build(inputs);
+                    }
+                    else if (subcommand == "interaction")
+                    {
+                        using comppi::controller::Interaction;
+                        Interaction controller(container);
+                        controller.build(inputs);
+                    }
+                    else if (subcommand == "localization")
+                    {
+                        using comppi::controller::Localization;
+                        Localization controller(container);
+                        controller.build(inputs);
+                    }
+                    else
+                    {
+                        std::cerr << "Unknown subcommand" << std::endl;
+                        return 1;
+                    }
+                } else {
+                    std::cout << "Usage: build subcommand" << std::endl;
+                    return 0;
+                }
+            } else {
+                std::cerr << "Unknown command" << std::endl;
+                return 1;
+            }
+        } else {
+            std::cout << "Usage: " << argv[0] << " command" << std::endl;
+            return 0;
+        }
     }
 
     return 0;
