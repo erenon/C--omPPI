796ebfb126e8a8e2b8388858087b3c2482192757
erenon <erenon2@gmail.com>
2013-11-07 22:29:18 +0100

    build-database.sh added

---
+++ bin/build-database.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+defaultBuild="Debug"
+build=${1:-$defaultBuild}
+
+bindir="$( cd "$( dirname "$0" )" && pwd )"
+bin=$bindir"/../"$build"/comppi --root "$bindir"/../"
+
+$bin build systemtype
+$bin build map
+$bin build interaction
+$bin build localization
