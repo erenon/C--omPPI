54a6079d1c40300962fb106213c3020e3ff7c59e
erenon <erenon2@gmail.com>
2013-11-07 21:46:51 +0100

    generate single .sql file, remove obsolate ones.

--- bin/generate-entities.sh
+++ bin/generate-entities.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# TODO generate single SQL file:
-# odb ... --generate-schema-only --at-once --input-name company position.hxx employee.hxx
-
 # TODO generate uncommented fkeys:
 # --fkeys-deferrable-mode not_deferrable <- unrecognized switch
 
 # TODO review odb options (e.g: --omit-*)
 
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 entity1.h [entity2.h [...]]"
+    exit
+fi
+
 echo "Generate ODB entities"
+
+bindir="$( cd "$( dirname "$0" )" && pwd )"
+
+changed=false
 
 for input in $@; do
     echo -n "check:" $input "... "
@@ -17,16 +23,16 @@ for input in $@; do
         ! -f "$(dirname $input)/gen/$(basename $input '.h')-odb.cxx" \
     -o  ! -f "$(dirname $input)/gen/$(basename $input '.h')-odb.hxx" \
     -o  ! -f "$(dirname $input)/gen/$(basename $input '.h')-odb.ixx" \
-    -o  ! -f "$(dirname $input)/gen/$(basename $input '.h').sql" \
     \
     -o  $input -nt "$(dirname $input)/gen/$(basename $input '.h')-odb.cxx" \
     -o  $input -nt "$(dirname $input)/gen/$(basename $input '.h')-odb.hxx" \
     -o  $input -nt "$(dirname $input)/gen/$(basename $input '.h')-odb.ixx" \
-    -o  $input -nt "$(dirname $input)/gen/$(basename $input '.h').sql" \
     ]; then
         echo "generate"
         
-        odb -d mysql --std c++11 --generate-schema --generate-query \
+        $changed = true
+        
+        odb -d mysql --std c++11 --generate-query \
         --guard-prefix COMPPI_ENTITY_GEN_ \
         --include-with-brackets --include-prefix comppi/entity/ \
         --include-regex "%comppi/entity/(.+)-odb(.+)%comppi/entity/gen/\1-odb\2%" \
@@ -38,3 +44,12 @@ for input in $@; do
         echo "nop"
     fi
 done;
+
+if [ $changed ]; then
+    odb -d mysql --std c++11 \
+    --generate-schema-only --at-once \
+    -x -I$bindir"/../src/" \
+    --output-name comppi \
+    --output-dir $bindir \
+    $@
+fi
